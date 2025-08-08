49305c8a1958143a54ad12cb06921c847d89fbec
IvanDrv <ivander05@mail.ru>
2025-08-08 22:06:03 +0300

    Minister implementation

---
+++ Lab4 - polymorphism and abstract classes/Minister.cpp
@@ -0,0 +1,13 @@
+#include "Minister.h"
+#include <iostream>
+
+std::string Minister::getPosition() const
+{
+    if (isPrime) return "Prime minister";
+        return "Minister: " + this->min;
+};
+
+std::string Minister::getDescription() const
+{
+    return "Party: " + this->party + " Ministry: " + this->min;
+};
